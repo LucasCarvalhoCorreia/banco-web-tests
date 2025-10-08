d0970d48ba2012a8566a4bb375c7a542983c5405
Lucas Correia <lucas.carvalho79@hotmail.com>
2025-10-08 20:20:18 -0300

    Adicionando comandos de screenshots e video na execução

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
+
+// npm run rest-api - banco-api
+// npm run server - banco-web
