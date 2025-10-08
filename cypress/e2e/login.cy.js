c2d2836c51a61ca6e1432c11615de2c43355c475
Lucas Correia <lucas.carvalho79@hotmail.com>
2025-10-08 20:34:05 -0300

    Adicionando variaveis de valore de usuario e senha

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,10 @@ describe('login', () => {
 
   it('Login com dados validos deve permitir entrada no sistema', () => {
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.screenshot('apos-preencher-o-formulario')
     cy.contains('button', 'Entrar').click()
     cy.screenshot('apos-clicar-no-botao-entrar')
@@ -19,11 +21,15 @@ describe('login', () => {
 
   it('Login com dados invalidos deve apresentar mensagem de erro', () => {
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button', 'Entrar').click()
 
     //Assert
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+
+    cy.wait(3000)
   })
 })
